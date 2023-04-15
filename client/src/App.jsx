3c7633dd96e5043b67d1c6d06c10e2f58c1eeb05
Ali Zulqarnain <warraichleopard@gmail.com>
2023-04-16 02:54:06 +0500

    Multiple users support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,81 @@
-import { useState } from 'react';
-import { io } from 'socket.io-client';
-import './App.css';
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
 
-const socket = io(
-  'https://vitejsvitehqenx5-qxda--8080--c8358679.local-credentialless.webcontainer.io/'
-);
-
-socket.on('connect', () => {
-  console.log(socket.id);
-  socket.emit('custom', { msg: 'Hello from the client!' });
-});
+const socket = io("http://localhost:8080");
 
 function App() {
-  const [msg, setMsg] = useState('');
-  const [id, setId] = useState(null);
-  const [messages, setMessages] = useState([]);
+    const [msg, setMsg] = useState("");
+    const [user, setUser] = useState({});
+    const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [receiver, setReceiver] = useState("");
 
-  socket.on('received', (msg) => {
-    setMessages([...messages, msg]);
-  });
+    socket.on("received", (msg) => {
+        setMessages([...messages, msg]);
+    });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessages([...messages, msg]);
-    socket.emit('msg', msg);
-    setMsg('');
-  };
+    socket.on("users", (users) => {
+        console.log(users);
+        setUsers(users.filter((item) => item.name !== user.name));
+    });
 
-  return (
-    <div className="App">
-      <h1>Socket IO</h1>
-      <p>Logged in with ID: {id || null}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={(e) => setMsg(e.target.value)}
-          value={msg}
-          style={{ width: '40%' }}
-        />
-        <button style={{ width: '20%' }}>Send message</button>
-      </form>
-      <div>
-        {messages.map((msg, index) => (
-          <p key={index}>{msg}</p>
-        ))}
-      </div>
-    </div>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessages([...messages, msg]);
+        socket.emit("msg", { msg, id: socket.id, receiver: receiver || "" });
+        setMsg("");
+    };
+
+    useEffect(() => {
+        socket.on("connect", () => {
+            const name = prompt("Enter your name");
+            setUser({ name, id: socket.id });
+            if (!name) return;
+            socket.emit("custom", {
+                msg: "Hello from the client!",
+                id: socket.id,
+                name,
+            });
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Socket IO</h1>
+            <p>Logged in: {user?.name || null}</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    onChange={(e) => setMsg(e.target.value)}
+                    value={msg}
+                    style={{ width: "40%" }}
+                />
+
+                {/* create a dropdown checkbox containing values from the users state */}
+                <select name="users" id="users" style={{ width: "25%" }}>
+                    {users.map((user) => (
+                        <option
+                            key={user?.id}
+                            value={user?.id}
+                            onChange={() => setReceiver(user?.id)}
+                        >
+                            {user.name}
+                        </option>
+                    ))}
+                </select>
+
+                <br />
+
+                <button style={{ width: "20%" }}>Send message</button>
+            </form>
+            <div>
+                {messages.map((msg, index) => (
+                    <p key={index}>{msg}</p>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- socket/main.js
+++ socket/main.js
@@ -1,24 +1,40 @@
-const express = require('express');
+const express = require("express");
+const { Server } = require("socket.io");
 const app = express();
 
 const port = 8080;
-const io = require('socket.io')(port, {
-  cors: {
-    origin: ['https://localhost:5173'],
-  },
+const io = new Server(port, {
+    cors: {
+        origin: ["http://localhost:5173"],
+    },
 });
 
+// to save connected users
+let users = [];
+
 // on connection establish
-io.on('connection', (socket) => {
-  console.log(socket.id || null);
-  socket.on('custom', (obj) => {
-    console.log(obj);
-  });
-  socket.on('msg', (msg) => {
-    socket.broadcast.emit('received', msg);
-  });
+io.on("connection", (socket) => {
+    // on user join
+    socket.on("custom", (user) => {
+        // if users array does not contain a user having same name, push the user object into users array
+        users = [...users.filter((prev) => prev.name !== user.name), user];
+        console.log(users);
+        console.log("Total clients connected: ", users.length);
+        io.emit("users", users);
+    });
+
+    // on message received
+    socket.on("msg", (obj) => {
+        socket.to(obj.id).emit("received", obj.msg);
+    });
+
+    // on user disconnect
+    socket.on("disconnect", () => {
+        users = users.filter((user) => user.id !== socket.id);
+        console.log("Total clients connected: ", users.length);
+    });
 });
 
 app.listen(8000, () => {
-  console.log('Server listening on port 8000');
+    console.log("Server listening on port 8000");
 });
